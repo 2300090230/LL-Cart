d096b4f631ceb304bc72318f247cd18920156c74
Laxman <2300090230@kluniversity.in>
2025-11-16 23:43:50 +0530

    v4.0

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = 
 {   
-    "url":"https://llcartbackend.onrender.com"
-    //"url":"http://localhost:2004"
+    //"url":"https://llcartbackend.onrender.com"
+    "url":"http://localhost:2004"
     //"https://sdpbackend-famf.onrender.com"
 }
 
