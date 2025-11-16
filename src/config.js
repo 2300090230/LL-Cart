a22c10d6967559a9dc77d401cd532cc302082982
Laxman <2300090230@kluniversity.in>
2025-11-16 23:19:50 +0530

    vercel1.0

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = 
 {   
-    "url":"https://llsdpbackend.onrender.com"
+    "url":"https://llcartbackend.onrender.com"
     //"url":"http://localhost:2004"
     //"https://sdpbackend-famf.onrender.com"
 }
